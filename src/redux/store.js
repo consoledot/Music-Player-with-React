eb8ac04b2801b9b755ac048b6e174aff0eb03a04
consoledot <bimbooladele@outlook.com>
2021-04-05 13:04:16 +0100

    remove redux logger from production and testing env

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
-import {createStore,applyMiddleware} from 'redux'
-import logger from 'redux-logger'
-import reducer from './reducer'
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import reducer from "./reducer";
 
+const middleware = [];
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
+const store = createStore(reducer, applyMiddleware(...middleware));
 
-const middleware = [logger]
-const store = createStore(reducer, applyMiddleware(...middleware))
-
-export default store
+export default store;
